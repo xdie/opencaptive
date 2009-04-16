2ae7596656e290d32e4b69d5e890bb3f87d40727
xdie <xdie@a6d01955-a1d4-4d1c-9c7a-22cde65e90db>
2009-04-16 14:04:17 +0000

    Ponemos a que loggee toda la basura que tira el squid para despues analizarla.

--- ip.php
+++ ip.php
@@ -1,18 +1,27 @@
 #!/usr/local/bin/php
 
 <?php
-include("db.php");
 
-// loop principal
+    /*****************************************************************
+     *Este script , es el famoso ACL Helper del Squid		     *
+     *Falta revision no funciona del todo bien, el squid tira errores*
+     *de lectura vacios, aun sigo esperando que me contesten. quien sabe :)			     *
+     *****************************************************************/
+
+
+include("db.php"); // Conectamos a la bd
+include("functions.php");// loop principal
+
 while (!feof(STDIN)) {
+
         $line = trim(fgets(STDIN));
         $ip = rawurldecode($line);
 	checkIp($ip);	
-    
 }
 
 
 // Chekeamos que si la ip se encuentra en la base!
+
 function checkIp($ip) {
 
 $ip = mysql_escape_string($ip);
@@ -24,14 +33,15 @@ while ($row=mysql_fetch_array($result)) {
 
 if ($row['ip'] == $ip) {
     fwrite(STDOUT, "OK user=".$row['username']."\n"); // si la encuentra devolvemos el username
+    logg("Info","Ok returned from iphelper to: ".$row['username']);
     return true;
 }  
 
 
 }
+
 fwrite(STDOUT, "ERR"."\n"); // no encontro nada, lo pateamos
-
-
+    logg("Error","ERR returned from iphelper ");
 sleep(2);
 }
 
