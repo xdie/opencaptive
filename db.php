885b9e6b4346bcc1ca75240218fe175b27f1aec0
xdie <xdie@a6d01955-a1d4-4d1c-9c7a-22cde65e90db>
2009-05-22 20:47:12 +0000

    Actualizamos el db.php, para que escriba en el log en caso de fallar al conectar o seleccionar la bd

--- db.php
+++ db.php
@@ -1,16 +1,21 @@
 <?php
 
+/* Este archivo solo conecta con la base de datos :)*/
+
 include("config.php");
+include("functions.php");
 
 $dbsock = mysql_connect($dbhost, $dbuser, $dbpass);
 
 if(!$dbsock) {
+	logg("DB",mysql_error());
         die("Cannot connect. " . mysql_error());
 }
 
 $dbselect = mysql_select_db($dbname);
 
 if(!$dbselect) {
+	logg("DB",mysql_error());
         die("Cannot select database " . mysql_error());
 }
 
