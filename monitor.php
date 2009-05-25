f2b9417794ac187b111170701aa6da265f97758e
xdie <xdie@a6d01955-a1d4-4d1c-9c7a-22cde65e90db>
2009-05-25 20:52:33 +0000

    parseamos el final del log, en caso de que el redirector falle mas de una vez

--- monitor.php
+++ monitor.php
@@ -1,6 +1,10 @@
 <?php
-include("functions.php");
+
 // Este es un monitor para detectar cuadno falla squidguard y nos deja canilla libre de internet
+
+include("functions.php");
+
+
 
 $today = date("Y/m/d"); // fecha actual para buscar en el log de hoy
 
@@ -11,16 +15,20 @@ $cmd = shell_exec($filter);
 
 if (!empty($cmd)){
 
-logg("ERROR","Problemas con el redirector SquidGuard! revisar configuracion!");
-
 $slog = "tail /var/squidguard/squidGuard.log"; // sacamos un fragmentillo del log
+
 $cmd = shell_exec($slog);
+
+if (ereg("emergency", $cmd)) { // buscamos si el error esta en el final del log, en caso de que falle mas de una vez por dia xD
+
+logg("ERROR","Problemas con el redirector SquidGuard! funcionando en modo de emergencia!");
 
 alertMail("Alerta! fallo el redirector","El redirector squidGuard no esta funcionando !\n\n".$cmd);
 
 } else {
 
-logg("Monitor","Probablemente el squidguard este funcionando de maravilla");
+logg("Info","Probablemente el squidguard este funcionando de maravilla");
+}
 
 }
 ?>
